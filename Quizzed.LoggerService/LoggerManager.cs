84dc3423569f7c1b43a0588367cf948584b2408b
Justin Scroggins <justinscroggins@outlook.com>
2021-11-06 14:01:37 -0400

    Wrap LoggerManager with NLog methods

--- Quizzed.LoggerService/LoggerManager.cs
+++ Quizzed.LoggerService/LoggerManager.cs
@@ -1,8 +1,32 @@
 ﻿using System;
+using NLog;
+using Quizzed.Contracts;
 
 namespace Quizzed.LoggerService
 {
-    public class LoggerManager
+    public class LoggerManager : ILoggerManager
     {
+        private static ILogger logger = LogManager.GetCurrentClassLogger();
+
+        public LoggerManager() {}
+        public void LogDebug(string message)
+        {
+            logger.Debug(message);
+        }
+
+        public void LogError(string message)
+        {
+            logger.Error(message);
+        }
+
+        public void LogInfo(string message)
+        {
+            logger.Info(message);
+        }
+
+        public void LogWarning(string message)
+        {
+            logger.Warn(message);
+        }
     }
 }
